f5765625664bdac926f48622fbd9b38b1cfd536c
vermellon <bowernika@gmail.com>
2020-06-04 10:02:48 -0400

    hotfixes for project 5

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const formElementEdit = document.querySelector('.popup__form_edit')
-const formElementAdd = document.querySelector('.popup__form_add')
+const formElementEdit = document.querySelector('.popup__form_edit');
+const formElementAdd = document.querySelector('.popup__form_add');
 const editBtn = document.querySelector('.profile__edit');
-const addBtn = document.querySelector('.profile__add')
+const addBtn = document.querySelector('.profile__add');
 const popupFormEdit = document.querySelector('.popup_form-edit');
 const popupFormAdd = document.querySelector('.popup_form-add');
 const popupImage = document.querySelector('.popup_image');
-const closePopupFormEdit = document.querySelector('.popup__close_form-edit')
-const closePopupFormAdd = document.querySelector('.popup__close_form-add')
-const closePopupImage = document.querySelector('.popup__close_image')
+const closePopupFormEdit = document.querySelector('.popup__close_form-edit');
+const closePopupFormAdd = document.querySelector('.popup__close_form-add');
+const closePopupImage = document.querySelector('.popup__close_image');
 const nameInput = document.querySelector('.popup__field_name');
 const aboutInput = document.querySelector('.popup__field_about');
 const titleInput = document.querySelector('.popup__field_title');
-const linkInput = document.querySelector('.popup__field_link')
-const nameOutput = document.querySelector('.profile__text')
-const aboutOutput = document.querySelector('.profile__occupation')
+const linkInput = document.querySelector('.popup__field_link');
+const nameOutput = document.querySelector('.profile__text');
+const aboutOutput = document.querySelector('.profile__occupation');
 const imagesList = document.querySelector(".images__list");
 
 
@@ -49,51 +49,49 @@ const initialCards = [{
   }
 ];
 
-function renderCards(cardsArray) {
-  imagesList.innerHTML = "";
-  cardsArray.forEach(element => imagesList.append(createCard(element.name, element.link)));
+function deleteImage(evt) {
+  evt.stopPropagation()
+  evt.target.parentNode.parentNode.remove()
+}
+
+function toggleDisplayImage(evt) {
+  popupImage.classList.toggle("popup_hidden")
+  const url = evt.target.style.backgroundImage
+  const caption = evt.target.parentNode.querySelector(".images__text").textContent
+  popupImage.querySelector(".images__picture_fullscreen").src = url.substring(5, url.length - 2);
+  popupImage.querySelector(".images__text_fullscreen").textContent = caption;
+  popupImage.querySelector(".images__picture_fullscreen").alt = caption;
 }
 
 function createCard(name, link) {
   const imageTemplate = document.querySelector("#images__card").content;
   const imageElement = imageTemplate.cloneNode(true);
-  const imgUrl = `url('${link}')`
-  const imagesPicture = imageElement.querySelector(".images__picture")
-  const deleteBtn = imageElement.querySelector(".images__delete")
+  const imgUrl = `url('${link}')`;
+  const imagesPicture = imageElement.querySelector(".images__picture");
+  const deleteBtn = imageElement.querySelector(".images__delete");
   imagesPicture.style.backgroundImage = imgUrl;
-  deleteBtn.addEventListener("click", evt => {
+  deleteBtn.addEventListener("click", (evt) => {
     deleteImage(evt)
-  }, false)
-
-  imageElement.querySelector(".images__picture").addEventListener("click", evt => {
-    toggleDisplayImage(evt, )
+  }, false);
+  imageElement.querySelector(".images__picture").addEventListener("click", (evt) => {
+    toggleDisplayImage(evt);
   })
   imageElement.querySelector(".images__text").textContent = name;
-
-  imageElement.querySelector(".images__like").addEventListener("click", evt => {
+  imageElement.querySelector(".images__like").addEventListener("click", (evt) => {
     evt.target.classList.toggle("images__like_active");
   })
   return imageElement
 }
 
-function deleteImage(evt) {
-  evt.stopPropagation()
-  evt.target.parentNode.parentNode.remove()
+function renderCards(cardsArray) {
+  imagesList.innerHTML = "";
+  cardsArray.forEach((element) => imagesList.append(createCard(element.name, element.link)));
 }
 
 function toggleDisplayForm(evt, form) {
   evt.preventDefault();
   form.classList.toggle("popup_hidden")
 }
-
-function toggleDisplayImage(evt) {
-  popupImage.classList.toggle("popup_hidden")
-  const url = evt.path[0].style.backgroundImage
-  popupImage.querySelector(".images__picture_fullscreen").src = url.substring(5, url.length - 2);
-  popupImage.querySelector(".images__text_fullscreen").textContent = evt.path[1].outerText;
-  popupImage.querySelector(".images__picture_fullscreen").alt = evt.path[1].outerText;;
-}
-
 
 function editProfileForm(evt) {
   nameInput.value = nameOutput.textContent;
